56a45782741b8abefb6b1db809dc57d6510052fb
Neil Everette <neil.everette@gmail.com>
2013-03-15 21:34:00 -0700

    Fixed editing the list header
    Fixed new and existing header

--- script2.js
+++ script2.js
@@ -2,28 +2,42 @@ $(document).ready(function(){
 
 	var newTitle = $('#listName');
 	var addButton = $('#addButton');
-	var existingTitle = $('#existingTitle');
 
 	function existingTitleClick(){
-		console.log("I work");
-	}
-
+		$('#existingTitle').click(function(){
+			$('#existingTitle').replaceWith('<input type="text" id="inputField" class = "input" name="listName"  value= "'+ $('#existingTitle').text() + '">');
+			$('#inputField').focus(); // PUTS INPUT INTO FOCUS
+			$('#inputField').focusout(function(){  // SETS TITLE ON CLICK OUT OF INPUT BOX
+				$('#inputField').replaceWith('<span id="existingTitle" class = "title"><p>' + $('#inputField').val() + '</p></span>');
+				existingTitleClick();
+		}); //REPLACES TITLE ON CLICK OUT
+			$(document).keypress(function(e) {  // SETS TITLE ON ENTER KEY
+   				if(e.keyCode == 13) {
+        		$('#inputField').replaceWith('<span id="existingTitle" class = "title"><p>' + $('#inputField').val() + '</p></span>');
+    	}
+    			existingTitleClick();
+    		}); // REPLACES TITLE ON ENTER
+		});
+		
+		
+	} // EDIT AN EXISTING TITLE
+	
 	newTitle.click(function(){
 		newTitle.replaceWith('<input type="text" id="inputField" class = "input" name="listName"  value= "">');
 		$('#inputField').focus(); // PUTS INPUT INTO FOCUS
 		$('#inputField').focusout(function(){  // SETS TITLE ON CLICK OUT OF INPUT BOX
-			$('#inputField').replaceWith('<span id="listName" class = "title">' + $('#inputField').val() + '</span>');
+			$('#inputField').replaceWith('<span id="existingTitle" class = "title"><p>' + $('#inputField').val() + '</p></span>');
 			existingTitleClick();
 		}); //REPLACES TITLE ON CLICK OUT
 		$(document).keypress(function(e) {  // SETS TITLE ON ENTER KEY
    			if(e.keyCode == 13) {
-        		$('#inputField').replaceWith('<span id="existingTitle" class = "title">' + $('#inputField').val() + '</span>');
+        		$('#inputField').replaceWith('<span id="existingTitle" class = "title"><p>' + $('#inputField').val() + '</p></span>');
     	}
     		existingTitleClick();
     	}); // REPLACES TITLE ON ENTER
     	
-	});	
-
+	});	// ADD A NEW TITLE
+	
 	addButton.click(function(){
 	//console.log("Add Button Clicked");
 	});	
